a572e7b2bb9c763dc50415226db7ad20f70e9332
akeramoi <akera.moi@gmail.com>
2018-09-02 05:01:09 -0400

    Initial entries - SGC

---
+++ SocialGoodCurrencyServices/Controllers/TransactionsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Web.Http;
+using SocialGoodCurrencyServices.Models;
+
+namespace SocialGoodCurrencyServices.Controllers
+{
+    public class TransactionsController : ApiController
+    {
+        
+        Transactions[] transactions = new Transactions[]
+        {
+            new Transactions { TransactionID = 1, CustomerID = 122332, TokenID = 233, ProductID = 1,  ActionAbbrev = "Whittle Me Bob", Amount= 12},
+            new Transactions { TransactionID = 2, CustomerID = 122332, TokenID = 234, ProductID = 2,  ActionAbbrev = "Down Under Mate", Amount= 100},
+            new Transactions { TransactionID = 3, CustomerID = 122332, TokenID = 235, ProductID = 3,  ActionAbbrev = "Up Fin Way", Amount= 1200},
+
+        };
+
+      
+
+        public IEnumerable<Transactions> GetAllTransactions()
+        {
+            return transactions;
+        }
+
+        public IHttpActionResult GetTransactions(int id)
+        {
+            var transaction = transactions.FirstOrDefault((p) => p.TransactionID == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+            return Ok(transaction);
+        }
+    }
+}
+
+
+
+
---
+++ SocialGoodCurrencyServices/Models/Transactions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SocialGoodCurrencyServices.Models
+{
+    public class Transactions
+    {
+
+
+        public int TransactionID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public int TokenID { get; set; } 
+
+        public int ProductID { get; set; } 
+
+        public DateTime ActionDate { get; set; }  
+
+        public string ActionAbbrev { get; set; } 
+
+        public double Amount { get; set; }
+
+    }
+}
